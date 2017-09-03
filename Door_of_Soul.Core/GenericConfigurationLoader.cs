402b67a94905c3fda659a12b9a2797f031eb93e8
DSobscure <snbruce13@yahoo.com.tw>
2017-09-03 22:23:18 +0800

    Entrance Proxy Scene ok

---
+++ Door_of_Soul.Core/GenericConfigurationLoader.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Door_of_Soul.Core
+{
+    public static class GenericConfigurationLoader<TConfiguration> where TConfiguration : class, new()
+    {
+        public static bool Load(string filePath, out TConfiguration configuration)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(TConfiguration));
+            if (File.Exists(filePath))
+            {
+                using (XmlReader reader = XmlReader.Create(filePath))
+                {
+                    if (serializer.CanDeserialize(reader))
+                    {
+                        configuration = (TConfiguration)serializer.Deserialize(reader);
+                        return true;
+                    }
+                    else
+                    {
+                        configuration = null;
+                        return false;
+                    }
+                }
+            }
+            else
+            {
+                TConfiguration defaultConfiguration = new TConfiguration();
+                using (XmlWriter writer = XmlWriter.Create(filePath))
+                {
+                    serializer.Serialize(writer, defaultConfiguration);
+                }
+                configuration = defaultConfiguration;
+                return false;
+            }
+        }
+    }
+}
